1d97fede7194dc2e776fb68e6556549b8f08137a
AleCaru02 <02alessandrocaruso@gmail.com>
2026-08-26 17:52:27 +0500

    style: load brand intelligence styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles.css";
 import "./scan.css";
 import "./content-generator.css";
+import "./brand.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
