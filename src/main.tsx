42ea93f5bddc4213f1d1cda283cf83fcc038f509
AleCaru02 <02alessandrocaruso@gmail.com>
2026-08-26 17:56:14 +0500

    style: load onboarding recovery styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./styles.css";
 import "./scan.css";
 import "./content-generator.css";
 import "./brand.css";
+import "./onboarding.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
